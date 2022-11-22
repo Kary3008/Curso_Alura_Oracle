7b855cd4833d7a177d5b3378bf36f19d358dc0b8
Kary <karyb3008@gmail.com>
2022-11-21 21:45:32 -0600

    Definir variables en JS

---
+++ JavaScript/Parte2/holaMundo.js
@@ -0,0 +1,10 @@
+//imprimir en consola
+console.log("Hola mundo");
+
+//definir variables
+var nombreEmpleado = "Kary B"; //variable
+//constante no cambia en el tiempo
+const nombre2 = "Diego";
+let direccion = "Av.105 - calle 3";
+var costoDelPasaje = 1500;
+let tasaEmbarque = 50.5;
