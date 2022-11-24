7b2571373804a39c89c989f424440c4b6da54973
Kary <karyb3008@gmail.com>
2022-11-24 09:39:45 -0600

    inner HTML, task.

--- JavaScript/DOM_js/script.js
+++ JavaScript/DOM_js/script.js
@@ -6,7 +6,19 @@ const createTask = (evento) =>
 {
     evento.preventDefault();
     const input = document.querySelector('[data-form-input]');
-    console.log(input.value);
+    const value = input.value;
+    const task = document.querySelector('[data-task]');
+    input.value = "";
+    //backticks  (template string)
+    const content = `<div>
+        <i class="far fa-check-square icon"></i>
+        <span class="task">${value}</span>
+        </div>
+        <i class="fas fa-trash-alt trashIcon icon"></i>`;
+    task.innerHTML = content; //agregar a HTML
+    console.log(value);
+    //console.log(task);
+    console.log(content);
 }
 
 //listener
