d5be6da54e775e3bf90161fef39a6e0c05773e05
Kary <karyb3008@gmail.com>
2022-11-25 13:19:53 -0600

    Cambiando las alertas de error.

--- JavaScript/WebJS/assets/js/validaciones.js
+++ JavaScript/WebJS/assets/js/validaciones.js
@@ -14,11 +14,51 @@ export function validar(input)
     {
         validadores[TipoInput](input);
     }
+
+//implementar input-container--invalid
+    //console.log(input.parentElement)
+    if(input.validity.valid)
+    {
+        input.parentElement.classList.remove("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = ''
+    }
+    else{
+        input.parentElement.classList.add("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = mostrarError(TipoInput, input)
+    }
 }
+
+const mensajesError = {
+    nombre: {
+        valueMissing: "Este campo no puede estar vacío"
+    },
+    email: 
+    {
+        valueMissing: "Este campo no puede estar vacío",
+        typeMismatch: "El correo no es válido"
+    },
+    password: 
+    {
+        valueMissing: "Este campo no puede estar vacío",
+        patternMismatch: "Al menos 6 caracteres, maximo 12, debe contener una letra mayúscula, una letra minúscula, un número y no puede contener caracteres especiales."
+    },
+    nacimiento: 
+    {
+        valueMissing: "Este campo no puede estar vacío",
+        customError: "Debes tener al menos 18 años de edad",
+    },
+};
 
 const validadores = { //arma un objeto para cada input -> fecha, nombre, etc.
     nacimiento: input => validarNac(input),
 };
+
+function mostrarError(TipoInput, input)
+{
+    let mensaje = ""
+
+    return mensaje;
+}
 
 function validarNac(input)
 {
